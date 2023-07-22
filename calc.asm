ba868760b0ac1553d7da5ba90ead7e4d2c0dc293
HAXOR <johannrplaster@gmail.com>
2023-07-22 17:02:38 -0500

    seperated functions into a library

--- calc.asm
+++ calc.asm
@@ -1,4 +1,5 @@
 ; Gets user input, converts to an actual number
+%include 'calcLibrary.asm'
 section .data
     msg     db      'Please enter a number: ', 0x0
     val     dw      0x0
@@ -26,83 +27,9 @@ _start:
     int     0x80
 
     mov     eax, ecx        ; parameter is string address
-    call _toInt
+    call    _toInt          ; converts string to int in eax
 
     quit:
-    ; Quit
     mov     eax, 1
     mov     ebx, 0
     int     0x80
-
-; toInt(factor (eax), string * (ecx))
-; returns int of string
-_toInt:
-    push    ebx
-    push    ecx
-
-    call _getFactor         ; gets factor for num of digits dynamically
-    nextChar1:
-    mov     ebx, eax        ; assign factor to ebx 
-
-    movzx   eax, byte [ecx] ; move first byte to eax
-    inc     ecx             ; move on to next byte
-
-    sub     eax, 48         ; convert to number
-    mul     ebx             ; factor num
-
-    push    ebx             ; save factor stored in ebx
-    mov     ebx, [val]
-    add     ebx, eax
-
-    mov     [val], ebx
-    pop     eax             ; pop factor saved in stack
-    call    _updateFactor   ; update factor 
-    jz      exitToInt 
-    jmp     nextChar1
-
-    exitToInt:
-    pop     ecx
-    pop     ebx
-    ret
-
-; updateFactor(factor (eax))
-; gets current factor and divides by 10
-; sets zero flag if zero
-_updateFactor:
-    push    ebx
-    push    edx
-
-    xor     edx, edx
-    mov     ebx, 10
-    div     ebx
-
-    cmp     eax, 0x0
-    pop     edx
-    pop     ebx
-    ret
-
-; getFactor(string* num (eax))
-; returns an inital factor by getting length
-_getFactor:
-    push    ebx
-    push    ecx
-    mov     ebx, eax        ; Create a reference point
-
-    nextChar2:
-    cmp     byte [eax], 0xa
-    je      done 
-    inc     eax
-    jmp     nextChar2
-    done:
-    sub     eax, ebx
-    mov     ecx, eax
-    mov     eax, 1
-    mov     ebx, 10
-    loop    factor
-    factor: 
-    mul     ebx
-    loop    factor
-
-    pop     ecx
-    pop     ebx
-    ret
